6b4941ffd88c3bf3f6c9a4443522b046fe61d321
fancy-snowman <mackan.holmberg@gmail.com>
2022-04-13 12:27:13 +0200

    Updated thirdparty path

--- demo-lua-ecs/demo/transform.lua
+++ demo-lua-ecs/demo/transform.lua
@@ -18,8 +18,7 @@ local function istransform(t)
 	return getmetatable(t) == transform
 end
 
-------------------------------------------------------------------- Meta events
-
+--------------------------------------------------------- Meta events
 function transform:__newindex(k, v)
 	print("transform - not possible to assign new fields")
 end
@@ -30,16 +29,13 @@ function transform:__tostring()
 		", scale" .. self.scale:__tostring()
 end
 
----------------------------------------------------------- Meta logic operators
-
+------------------------------------------------ Meta logic operators
 function transform.__eq(a, b)
 	assert(istransform(a) and istransform(b), "transform eq - expected args: transform, transform")
 	return a.position==b.position and a.rotation==b.rotation and a.scale==b.scale
 end
 
------------------------------------------------------------ Wrap up and send :)
-
+------------------------------------------------- Wrap up and send :)
 transform.new = new
 transform.istransform = istransform
-
 return setmetatable(transform, {__call = function(_,...) return new(...) end})
